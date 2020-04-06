99bc4cc756e6d3eeb2f2d6d653fba774b71df7fa
Abel Moro Paje <abelmoro@ucm.es>
2020-04-06 16:58:25 +0200

    Fix temporal para activar el escudo

--- Assets/Scripts/ActiveShield.cs
+++ Assets/Scripts/ActiveShield.cs
@@ -2,39 +2,30 @@
 
 public class ActiveShield : MonoBehaviour
 {
-    GameObject hand;
-    ObjectHealth shield;
+    //FIX TEMPORAL, NO TERMINADO
+    // Falta comprobar si hay un escudo, la vida de este etc...
+
+    GameObject hand;    
 
     void Start()
     {
-        hand = gameObject.transform.GetChild(0).GetChild(0).gameObject;
-        shield = hand.transform.GetChild(0).GetComponent<ObjectHealth>();
+        hand = gameObject.transform.GetChild(0).GetChild(0).gameObject;        
         hand.SetActive(false);
     }
 
     void Update()
-    {
-        // Si el escudo tiene vida...
-        if(shield.health > 0) 
-        {
-            // ... se pulsa el ratón y el escudo no está sacado...
-            if (Input.GetButtonDown("Fire1") && !hand.active)
+    {        
+            // ... se pulsa el ratón y el escudo no está activado
+            if (Input.GetButtonDown("Fire1") && !hand.activeSelf)
             {
                 // ... se activa el escudo
                 hand.SetActive(true);
             }
-            // ... y se suelta el ratón...
-            else if (Input.GetButtonUp("Fire1"))
+            // ... y se suelta el ratón y el escudo está activado
+            else if (Input.GetButtonUp("Fire1") && hand.activeSelf)
             { 
                 // ... el escudo se desactiva
                 hand.SetActive(false);
-            }
-        }
-        // Si el escudo no tiene vida y esta activo...
-        else if(hand.active)
-        {
-            // ... se desactiva
-            hand.SetActive(false);
-        }
+            }        
     }
 }
