f82cb7e79f8792ca6ed7bef9bef95e0baa75ea7f
EvSanz <62762680+EvSanz@users.noreply.github.com>
2020-04-15 11:05:56 +0200

    Script Area de Daño
    Este script quita vida al jugador poco a poco cuando este entra en su área de acción

---
+++ impactDamage.cs
@@ -0,0 +1,21 @@
+﻿//Este script se usa para todo objeto que hace daño al jugador o al escudo cuando colisiona con ellos
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class impactDamage : MonoBehaviour
+{
+    //Daño configurable desde el editor
+    public int damage;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Accedemos a la vida del jugador/escudo para luego poder reducirla
+        objectHealth hp = collision.gameObject.GetComponent<objectHealth>();
+        //Codigo defensivo
+        if (hp != null)
+        {
+            hp.applyDamage(damage);
+            Destroy(this.gameObject);
+        }
+    }
+}
