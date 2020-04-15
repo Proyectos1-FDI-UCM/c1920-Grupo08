3d975575c95d86b87efa39a2e044c84b21c83e9b
EvSanz <62762680+EvSanz@users.noreply.github.com>
2020-04-15 11:11:00 +0200

    Script Instant Kill
    Este script genera muerte instantánea en el jugador, aunque aún puede ser mejorado

---
+++ InstantKill.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantKill : MonoBehaviour
+{
+    public GameObject healthBar;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        objectHealth hp = other.gameObject.GetComponent<objectHealth>();
+        if (hp != null)
+        {
+            healthBar.GetComponent<valueBar>().SetValue(0);
+            Destroy(other.gameObject);
+        }
+
+    }
+}
