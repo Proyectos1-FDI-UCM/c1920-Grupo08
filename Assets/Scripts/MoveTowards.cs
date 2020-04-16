7747a83acc8257fb17476188507ff4f9a6e02fb0
Abel Moro Paje <abelmoro@ucm.es>
2020-04-16 02:41:50 +0200

    Primera iteración del nuevo dibujado de balas

---
+++ Assets/Scripts/MoveTowards.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowards : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform firepoint;
+    public Transform target;
+    Vector2 targetv;
+    Vector2 positionv;
+    float speed = 50f;
+
+    void Start()
+    {
+        targetv = target.position;
+        positionv = firepoint.position;
+        transform.position = positionv;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float fixedspeed= speed * Time.deltaTime;
+
+        transform.position = Vector2.MoveTowards(transform.position, targetv, fixedspeed);
+    }
+}
