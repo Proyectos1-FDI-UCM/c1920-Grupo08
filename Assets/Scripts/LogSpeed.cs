8a0d42b5f4d026aab8d5fac11c63be54794e5927
GonzaloFdezMoreno <gonzaf05@ucm.es>
2020-04-17 12:54:48 +0200

    Creada la escena de troncos

---
+++ Assets/Scripts/LogSpeed.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogSpeed : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * speed;
+    }
+
+    //Mueve el tronco a la velocidad de "Speed" 
+    void Update()
+    {
+        
+    }
+
+    
+    public void Rotate(Vector2 rotation)
+    {
+        transform.right = rotation;
+    }
+}
