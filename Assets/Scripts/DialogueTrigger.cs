501faa619b915f4bb780784b873b16ebc7f1a6e4
Abel Moro Paje <abelmoro@ucm.es>
2020-04-18 09:53:47 +0200

    Fix OnTriggerEnter2D

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -5,21 +5,16 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
     [Header("Consulta el index en el array del GM para elegir la frase")]
-    public int index = 0; 
-    public LayerMask s;
-    public float range = 0.5f;
-    private void FixedUpdate()
+    public int index = 0;
+    private int n = 0; 
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (Physics2D.OverlapCircle(transform.position, range, s))
+        if ((collision.gameObject.tag == "Player") && (n < 1))
         {
+            n += 1;
             GameManager.instance.OnDialogue(index);
             Destroy(this.gameObject);
         }
-    }
-
-    // Muestra el rango de disparo en el editor
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.DrawWireSphere(transform.position, range);
     }
 }
