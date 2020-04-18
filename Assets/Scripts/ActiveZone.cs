ce02a1cac84edd8c90e57071136f8a9fba3ec1b5
edekadt <edekadt@ucm.es>
2020-04-18 17:52:06 +0200

    Añadido las zonas de activación

---
+++ Assets/Scripts/ActiveZone.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Este script activa todos los objetos hijos del objeto asociado. Una vez activados,
+//la zona deja de registrar colisiones
+public class ActiveZone : MonoBehaviour
+{
+    BoxCollider2D col;
+    private void Start()
+    {
+        col = GetComponent<BoxCollider2D>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("step 1");
+        //Pasamos por todos los hijos, activándolos
+        for (int i = 0; i < transform.childCount; i++)
+            transform.GetChild(i).gameObject.SetActive(true);
+        //Desactivamos el collider para evitar que se repita
+        if (col != null)
+            col.enabled = false;
+    }
+}
