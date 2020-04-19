2f9a5324689326bfc2d407b9777a4f7f2adff9db
edekadt <edekadt@ucm.es>
2020-04-19 17:47:59 +0200

    Creado el script PickupHover
    Hace flotar lentamente un objeto sin rigidbody

---
+++ Assets/Scripts/Pickups/PickupHover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupHover : MonoBehaviour
+{
+    float aux;
+    float height;
+    void Start()
+    {
+        aux = 0;
+        height = transform.position.y;
+    }
+
+    void Update()
+    {
+        transform.Translate(0f, Mathf.Sin(aux)/90, 0f);
+        aux += 0.06f;
+    }
+}
