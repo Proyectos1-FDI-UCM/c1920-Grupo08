43c1a4f5418774cd79ae3111b71affc15acf7a5c
EvSanz <evsanc07@ucm.es>
2020-04-20 12:58:40 +0200

    Script Checkpoint
    Sirve para cambiar el checkpoint actual y almacenar su posicion

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    bool checkpointActivated = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerMove player = other.GetComponent<PlayerMove>();
+
+        if (player != null)
+        {
+            checkpointActivated = true;
+        }
+
+        Debug.Log("Checkpoint: " + checkpointActivated);
+    }
+}
