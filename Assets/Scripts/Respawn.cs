6655640a34e967a289779b1cf1f913aaec5e3471
EvSanz <evsanc07@ucm.es>
2020-04-20 12:59:27 +0200

    Script de Respawneo
    Este script permite que el jugador aparezca en la ubicacion del ultimo checkpoint

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    Vector2 respawnPoint;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Checkpoint checkpoint = other.GetComponent<Checkpoint>();
+
+        if (checkpoint != null)
+            respawnPoint = other.transform.position;
+    }
+
+    void PlayerLostLife ()
+    {
+        if (GameManager.instance != null)
+        {
+            if (GameManager.instance.PlayerDied() == true)
+                this.transform.position = respawnPoint;
+        }
+    }
+}
