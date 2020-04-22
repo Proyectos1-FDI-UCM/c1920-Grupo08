234472a81cf6e4873f96cc80a1ef59b531844d86
Abel Moro Paje <abelmoro@ucm.es>
2020-04-22 17:29:28 +0200

    repairkit script

---
+++ Assets/Scripts/Pickups/Repairkit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Repairkit : MonoBehaviour
+{
+
+    [SerializeField] private float reapairvalue = 20f;
+    [SerializeField] private Sound sound;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.GetComponent<PlayerMove>() != null)
+        {
+            if (Input.GetButtonDown("Use"))
+            {
+                AudioManager.instance.PlaySoundOnce(sound);
+                GameManager.instance.OnRepair(reapairvalue);
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
