6ce1e64bb6fdbe9a431bed889a89fa66eef291f6
Abel Moro Paje <abelmoro@ucm.es>
2020-04-22 17:12:08 +0200

    Integración con el AM

--- Assets/Scripts/Enemy/Bullet.cs
+++ Assets/Scripts/Enemy/Bullet.cs
@@ -6,17 +6,33 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class Bullet : MonoBehaviour
 {
+    [SerializeField] private Sound playerHit;
+    [SerializeField] private Sound shieldHit;
+    [SerializeField] private Sound groundHit;
+    AudioManager audioManager;
     private int damage = 10;
+
+    private void Start()
+    {
+        audioManager = AudioManager.instance;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<PlayerMove>() != null)
         {
             GameManager.instance.OnHit(collision.gameObject, damage);
+            audioManager.PlaySoundOnce(playerHit);
         }
 
-        if (collision.GetComponent<Shield>() != null)
+        else if(collision.GetComponent<Shield>() != null)
         {
             GameManager.instance.OnHit(collision.gameObject, damage);
+            audioManager.PlaySoundOnce(shieldHit);
+        }
+
+        else 
+        {
+            audioManager.PlaySoundOnce(groundHit);
         }
 
         Destroy(this.gameObject);
