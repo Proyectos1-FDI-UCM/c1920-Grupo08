26c600ad37ce37a3be2a14ddab7efba54015c138
Abel Moro Paje <abelmoro@ucm.es>
2020-04-26 12:43:34 +0200

    Añadido script DamageOverTime basado en DamageArea

---
+++ Assets/Scripts/Enemy/DamageOverTime.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class DamageOverTime : MonoBehaviour
+{
+    [SerializeField] private float damage;
+    private float damageTick = 1f;
+    private float timeElapsed = 0f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Player>() != null)
+        {
+            timeElapsed = Time.time;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if ((timeElapsed < Time.time) && (collision.GetComponent<Player>() != null))
+        {
+            GameManager.instance.OnHit(collision.gameObject, damage);
+            timeElapsed += damageTick;
+        }
+    }
+}
