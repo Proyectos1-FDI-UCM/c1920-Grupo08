362a4f3d5c226c3fc081f980c35bb2b58e33d2b8
Abel Moro Paje <abelmoro@ucm.es>
2020-04-26 13:25:13 +0200

    Refáctorizacion y mejora del comportamiento de los troncos y del spawner

---
+++ Assets/Scripts/Enemy/Log.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Log : MonoBehaviour
+{
+    void Start()
+    {        
+        Destroy(this.gameObject, 3f);
+    }
+
+    public void Update()
+    {        
+        transform.Rotate(0f, 0f, -2f);
+    }
+  
+}
---
+++ Assets/Scripts/Enemy/LogSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject logPrefab;
+    [SerializeField] private float spawnTime;
+    [SerializeField] private float warmup;
+    [SerializeField] private bool left = false;
+    [SerializeField] private float logSpeed;
+    protected Transform spawnPool;
+    private float moveX = 0;
+
+    //Invoca un tronco cada X tiempo
+    private void Start()
+    {
+        if (left)
+        {
+            moveX = -1;
+        }
+        else moveX = 1;
+
+        InvokeRepeating("SpawnLog", 3f, spawnTime);
+    }
+
+    public void SpawnLog()
+    {
+        GameObject log = Instantiate(logPrefab, transform.position, transform.rotation, spawnPool);
+        Rigidbody2D rb = log.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(moveX * logSpeed, rb.velocity.y);
+    }
+}
+    
+
