b21b7de14a89a2f5904fe4862227ce8b9986b61f
edekadt <edekadt@ucm.es>
2020-05-12 21:04:37 +0200

    Arreglada el script de puerta

--- Assets/Scripts/Pickups/Doors.cs
+++ Assets/Scripts/Pickups/Doors.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class Doors : MonoBehaviour
 {
+    [SerializeField]
+    bool locked=false;
+    BoxCollider2D bc;
+    private void Start()
+    {
+        bc = transform.GetChild(0).GetComponent<BoxCollider2D>();
+    }
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.GetComponent<PlayerMove>() != null)
         {
-            if (Input.GetButtonDown("Use"))
+            if (Input.GetButtonDown("Use") && (!locked || GameManager.instance.HasKey()))
             {
-                Destroy(this.gameObject);
+                bc.enabled = false;
+                //Change sprite
             }
         }
     }
