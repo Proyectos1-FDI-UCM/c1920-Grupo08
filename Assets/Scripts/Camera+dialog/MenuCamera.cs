98985fec69a17196d09329d4e16628204018f324
Abel Moro Paje <abelmoro@ucm.es>
2020-05-17 15:03:55 +0200

    Nuevo menu principal implementado

---
+++ Assets/Scripts/Camera+dialog/MenuCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCamera : MonoBehaviour
+{
+    [SerializeField] Transform pointA, pointB;
+    float speed = 0.5f;
+    Transform currentTarget;    
+  
+    void Update()
+    {
+        if (transform.position == pointA.position) currentTarget = pointB;
+        if (transform.position == pointB.position) currentTarget = pointA;
+        float movement = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, currentTarget.position, movement);
+    }
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] Panels;
+    AudioSource buttonSound;
+    SceneLoader sceneLoader;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonSound = GetComponent<AudioSource>();
+        sceneLoader = SceneLoader.instance;
+        SetPanel(0);
+    }
+
+    // Update is called once per frame
+    public void Continue()
+    {
+        buttonSound.Play();
+        SetPanel(1);
+    }
+
+    public void Play()
+    {
+        buttonSound.Play();
+        sceneLoader.NextScene();
+    }
+
+    public void Controls()
+    {
+        buttonSound.Play();
+        SetPanel(2);
+    }
+
+    public void Back()
+    {
+        buttonSound.Play();
+        SetPanel(1);
+    }
+
+    public void Exit()
+    {
+        buttonSound.Play();
+        sceneLoader.Exit();
+    }
+
+    void SetPanel(int index)
+    {
+        for (int i = 0; i < Panels.Length; i++)
+        {
+            if (i == index)
+            {
+                Panels[i].SetActive(true);
+            }
+            else
+            {
+                Panels[i].SetActive(false);
+            }
+        }
+    }
+}
