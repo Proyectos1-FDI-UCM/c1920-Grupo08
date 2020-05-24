88cee7d4ac51919e49c1940f772800776d27c333
edekadt <edekadt@ucm.es>
2020-05-24 12:43:20 +0200

    Añadido un sprite rodante al tronco

---
+++ Assets/Scripts/Enemy/Rotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    enum Direction { Left, Right };
+    [SerializeField]
+    Direction direction;
+    [SerializeField]
+    float rotateSpeed;
+    public void Update()
+    {   
+        if (direction==Direction.Right)
+            transform.Rotate(0f, 0f, -rotateSpeed);
+        else
+            transform.Rotate(0f, 0f, rotateSpeed);
+    }
+}
