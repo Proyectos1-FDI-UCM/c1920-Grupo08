aa386d111455372f33ac212a43f6aa45068fec0a
edekadt <edekadt@ucm.es>
2020-05-27 17:26:27 +0200

    Parcheado un fallo de ejecución
    Cuando el jugador se destruía, la cámara quedaba siguiendo un objeto inexistente

---
+++ Assets/Scripts/Player+Shield/DestroyCamOnDeath.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyCamOnDeath : MonoBehaviour
+{
+    private void OnDestroy()
+    {
+        Destroy(SmoothMovement.mainCamera);
+    }
+}
