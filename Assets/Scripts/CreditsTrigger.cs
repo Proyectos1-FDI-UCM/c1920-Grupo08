c1fefebdba428c77cde973508356d6b7111c0c18
Abel Moro Paje <abelmoro@ucm.es>
2020-05-27 05:38:10 +0200

    Añadido los créditos al nivel final

---
+++ Assets/Scripts/CreditsTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsTrigger : MonoBehaviour
+{
+    [SerializeField] GameObject credits;
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<PlayerController>() != null) 
+        {
+            credits.SetActive(true);
+            Destroy(this.gameObject);
+        }
+    }
+}
