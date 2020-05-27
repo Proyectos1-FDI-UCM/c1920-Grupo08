c72b940b8295f33626f48a06a9c468c5e0b8a45e
Abel Moro Paje <abelmoro@ucm.es>
2020-05-27 03:40:37 +0200

    Añadido script para cambiar de nivel al llegar al final del mismo

---
+++ Assets/Scripts/EndOfTheLevel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndOfTheLevel : MonoBehaviour
+{
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.GetComponent<PlayerController>() != null) 
+		{
+			SceneLoader.instance.NextScene();
+		}
+	}
+}
