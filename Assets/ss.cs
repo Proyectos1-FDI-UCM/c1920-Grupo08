734a32b669aa6352f73b5e2c12d7abe746db75a0
AbelMoro <abelmoro@ucm.es>
2020-05-29 02:36:47 +0200

    Primera versión de una escena de introducción al juego

---
+++ Assets/ss.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ss : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI bubbleText;
+    string dialogue;
+    private float typeSpeed = 0.1f;
+    AudioSource audioS;    
+
+    private void Start()
+    {
+        audioS = GetComponent<AudioSource>();
+        dialogue = bubbleText.text;
+        bubbleText.text = "";
+        StartCoroutine(Type());
+    }    
+
+    private IEnumerator Type()
+    {
+        foreach (char c in dialogue.ToCharArray())
+        {
+            bubbleText.text += c;
+            yield return new WaitForSeconds(typeSpeed);
+        }
+
+        audioS.Stop();        
+    }
+}
