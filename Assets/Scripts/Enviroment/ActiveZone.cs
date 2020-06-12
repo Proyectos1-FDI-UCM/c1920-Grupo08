019158fbcb50ad79ab0fd3bcb88323c95636bcf7
AbelMoro <abelmoro@ucm.es>
2020-06-12 04:44:47 +0200

    Pequeño fix en una instrucción obsoleta

--- Assets/Scripts/Enviroment/ActiveZone.cs
+++ Assets/Scripts/Enviroment/ActiveZone.cs
@@ -15,7 +15,7 @@ public class ActiveZone : MonoBehaviour
     {
         //Pasamos por todos los hijos, activándolos
         for (int i = 0; i < transform.childCount; i++)
-            transform.GetChild(i).gameObject.SetActive(!transform.GetChild(i).gameObject.active);
+            transform.GetChild(i).gameObject.SetActive(!transform.GetChild(i).gameObject.activeSelf);
         //Desactivamos el collider para evitar que se repita
         if (col != null)
             col.enabled = false;
